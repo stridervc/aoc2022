9938deb131726eee8666a530bc3c96f90515126a
Fred Strauss <fred@strider.co.za>
2022-12-05 18:37:59 +0200

    Day 06 prep

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,14 @@ module Main where
 
 import qualified Data.Map as M
 import System.Environment (getArgs)
+import System.Posix.Files (fileExist)
 
 import qualified Day01 (solve)
 import qualified Day02 (solve)
 import qualified Day03 (solve)
 import qualified Day04 (solve)
 import qualified Day05 (solve)
+import qualified Day06 (solve)
 
 solutions = M.fromList
   [ ("01", Day01.solve)
@@ -15,14 +17,18 @@ solutions = M.fromList
   , ("03", Day03.solve)
   , ("04", Day04.solve)
   , ("05", Day05.solve)
+  , ("06", Day06.solve)
   ]
 
 solveSingle :: String -> IO ()
 solveSingle day = do
   putStrLn $ "--- Day " ++ day ++ " ---"
-  case M.lookup day solutions of
-    Just solver -> readFile (concat ["./inputs/input", day, ".txt"]) >>= solver >> putStrLn ""
-    Nothing     -> putStrLn "Not yet implemented"
+  exist <- fileExist inputfile
+  if exist then case M.lookup day solutions of
+      Just solver -> readFile inputfile >>= solver >> putStrLn ""
+      Nothing     -> putStrLn "Not yet implemented"
+  else putStrLn "No input file"
+  where inputfile = concat ["./inputs/input", day, ".txt"]
 
 main :: IO ()
 main = do
